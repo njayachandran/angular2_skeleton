6fbcf1e362dd0d4a412790f51933f15c06f80892
Jayachandran Natarajan <jayachandran.rn@gmail.com>
2017-09-14 16:39:17 +0530

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>Skelator Project</h1>'
+  template: '<h1>Skelaton Project</h1>'
 })
 
 export class AppComponent {}
